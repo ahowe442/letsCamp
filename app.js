a85618f074e4693a479a9c572a75a457c9c53bfd
Abby H <howe.abigail@gmail.com>
2021-01-31 10:13:41 -0600

    Add joi depend., config server-side validation

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const methodOverride = require('method-override');
+const { join } = require('path');
 const catchAsync = require('./utilities/catchAsync');
-const expressError = require('./utilities/ExpressError');
-const Campground = require('./models/campground');
 const ExpressError = require('./utilities/ExpressError');
+const Campground = require('./models/campground');
+// const { privateEncrypt } = require('crypto');
 
 mongoose.connect('mongodb://localhost:27017/lets-camp', {
   useNewUrlParser: true,
@@ -43,8 +45,24 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', catchAsync(async (req, res) => {
+  // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required(),
+    }).required(),
+  });
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  }
+  console.log(result);
+
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
