228eff9154ca9c773f664357eef65a2da1cbec4a
Abby H <howe.abigail@gmail.com>
2021-02-07 11:34:38 -0600

    fix reset command

--- models/review.js
+++ models/review.js
@@ -1,7 +1,8 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable import/newline-after-import */
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   body: String,
