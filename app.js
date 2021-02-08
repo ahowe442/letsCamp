da85d25fd5b3eea6ee99f12ea17ace9c0b5223b0
Abby Howe <howe.abigail@gmail.com>
2021-02-07 22:52:39 -0600

    Add validation for user reviews

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
@@ -8,7 +9,7 @@ const methodOverride = require('method-override');
 const Joi = require('joi');
 const { join } = require('path');
 const { privateEncrypt } = require('crypto');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
 const Campground = require('./models/campground');
@@ -45,6 +46,16 @@ const validateCampground = (req, res, next) => {
   }
 };
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -70,7 +81,7 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   res.render('campgrounds/show', { campground });
 }));
 
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+app.get('/campgrounds/:id/edit', validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
 }));
