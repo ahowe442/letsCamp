65c2035e857ce9bf4d816543cfde933c853b6cd8
Abby H <howe.abigail@gmail.com>
2021-02-12 19:17:58 -0600

    EOD - Buggy code routes/reviews.js issue: TODO: fix

--- schemas.js
+++ schemas.js
@@ -4,7 +4,8 @@ const { number } = require('joi');
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
-    price: Joi.number().required().min(0),
+    // TODO: Requiring a number for price results in bug.
+    price: Joi.number(),
     image: Joi.string().required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
@@ -13,7 +14,7 @@ module.exports.campgroundSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required(),
+    rating: Joi.number().required().min(1).max(5),
     body: Joi.string().required(),
   }).required(),
 });
